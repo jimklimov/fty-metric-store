c042d5f7268a5122ee79c10b37c6ab9110fd3bf7
barraudl <lilianbarraud@eaton.com>
2019-01-23 09:27:20 -0500

    Update to use new shm's interface
    Signed-off-by: barraudl <lilianbarraud@eaton.com>

--- src/fty_metric_store_server.cc
+++ src/fty_metric_store_server.cc
@@ -483,7 +483,7 @@ fty_metric_store_metric_pull (zsock_t *pipe, void* args)
         if (zpoller_expired (poller)) {
           fty::shm::shmMetrics result;
           log_debug("read metrics");
-          fty::shm::read_metrics(FTY_SHM_METRIC_TYPE, ".*", ".*",  result);
+          fty::shm::read_metrics(".*", ".*",  result);
           log_debug("metric reads : %d", result.size());
           g_row_mutex.lock();
           s_process_metrics(result);
